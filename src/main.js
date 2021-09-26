c7495e354caf16772cd8a00e048f4c1208dbae10
mprajeesh <mprajeesh@outlook.com>
2021-09-26 06:11:27 +0530

    Added Vuex

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import translation from "../public/assets/languages/en.json"
 import trace from "@/utilities/debug.js"
+import store from './store'
 
 Vue.config.productionTip = false
 window.$translation = translation;
 window.trace = trace;
 
 new Vue({
-  render: h => h(App),
+  store,
+  render: h => h(App)
 }).$mount('#app')
